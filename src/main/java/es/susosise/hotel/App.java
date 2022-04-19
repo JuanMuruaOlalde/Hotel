7c53fdfc21dfe654d0037c15708cda186cf02829
Juan Murua Olalde <juan@susosise.es>
2022-04-19 19:19:47 +0200

    esqueleto basico de Maven

---
+++ src/main/java/es/susosise/hotel/App.java
@@ -0,0 +1,13 @@
+package es.susosise.hotel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
