8c29dabbb8e30de6868f41edc696a063c7a28ee8
Juan Murua Olalde <juan@susosise.es>
2022-04-29 16:55:53 +0200

    SQL: implementar la recuperacion de todas las tablas     y arreglar la forma de cerrar resultsets y comandos

--- src/main/java/es/susosise/hotel/habitaciones/PersistenciaDeHabitacionesEnBaseDeDatosSQL.java
+++ src/main/java/es/susosise/hotel/habitaciones/PersistenciaDeHabitacionesEnBaseDeDatosSQL.java
@@ -2,6 +2,7 @@ package es.susosise.hotel.habitaciones;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,9 +28,13 @@ public class PersistenciaDeHabitacionesEnBaseDeDatosSQL implements PersistenciaD
              ;
     }
     protected void crearLaTabla() throws SQLException {
-        java.sql.Statement comando = baseDeDatos.createStatement();
-        comando.execute(getSQLparaCrearLaTabla());
-        comando.close();
+        java.sql.Statement comando = null;
+        try {
+            comando = baseDeDatos.createStatement();
+            comando.execute(getSQLparaCrearLaTabla());
+        } finally {
+            try { if (comando != null) comando.close(); } catch (Exception ex) {}
+        }
     }
     
     
@@ -53,13 +58,16 @@ public class PersistenciaDeHabitacionesEnBaseDeDatosSQL implements PersistenciaD
         sentenciaSQL.append("      tipoDeBaño = ");
         sentenciaSQL.append("'" + unaHabitacion.getTipoDeBaño().toString() + "'");
         sentenciaSQL.append(System.lineSeparator());
+        
+        java.sql.Statement comando = null;
         try {
-            java.sql.Statement comando = baseDeDatos.createStatement();
+            comando = baseDeDatos.createStatement();
             comando.execute(sentenciaSQL.toString());
-            comando.close();
         } catch(SQLException ex) {
             throw new IOException("Error al añadir la habitacion " + unaHabitacion.getNumeroDeHabitacion() 
                                    + " a la base de datos.");
+        } finally {
+            try { if (comando != null) comando.close(); } catch (Exception ex) {}
         }
     }
 
@@ -72,9 +80,12 @@ public class PersistenciaDeHabitacionesEnBaseDeDatosSQL implements PersistenciaD
         sentenciaSQL.append("  WHERE idInterno = ");
         sentenciaSQL.append("'" + id.toString() + "'");
         sentenciaSQL.append(System.lineSeparator());
+        
+        java.sql.Statement comando = null;
+        java.sql.ResultSet respuesta = null;
         try {
-            java.sql.Statement comando = baseDeDatos.createStatement();
-            java.sql.ResultSet respuesta = comando.executeQuery(sentenciaSQL.toString());
+            comando = baseDeDatos.createStatement();
+            respuesta = comando.executeQuery(sentenciaSQL.toString());
             if (respuesta.next()) {
                 habitacion = new Habitacion(java.util.UUID.fromString(respuesta.getString("idInterno")),
                                             respuesta.getBoolean("activa"),
@@ -89,7 +100,10 @@ public class PersistenciaDeHabitacionesEnBaseDeDatosSQL implements PersistenciaD
             System.out.println("Error al recuperar de la base de datos "
                              + "la habitacion con código " + id.toString()
                              + System.lineSeparator() + System.lineSeparator()
-                             + ex.getStackTrace().toString());
+                             + Arrays.toString(ex.getStackTrace()));
+        } finally {
+            try { if (respuesta != null) respuesta.close(); } catch (Exception ex) {}
+            try { if (comando != null) comando.close(); } catch (Exception ex) {}
         }
         return habitacion;
     }
@@ -103,9 +117,12 @@ public class PersistenciaDeHabitacionesEnBaseDeDatosSQL implements PersistenciaD
         sentenciaSQL.append("  WHERE numeroDeHabitacion = ");
         sentenciaSQL.append("'" + numeroDeHabitacion + "'");
         sentenciaSQL.append(System.lineSeparator());
+        
+        java.sql.Statement comando = null;
+        java.sql.ResultSet respuesta = null;
         try {
-            java.sql.Statement comando = baseDeDatos.createStatement();
-            java.sql.ResultSet respuesta = comando.executeQuery(sentenciaSQL.toString());
+            comando = baseDeDatos.createStatement();
+            respuesta = comando.executeQuery(sentenciaSQL.toString());
             if (respuesta.next()) {
                 habitacion = new Habitacion(java.util.UUID.fromString(respuesta.getString("idInterno")),
                                             respuesta.getBoolean("activa"),
@@ -120,15 +137,46 @@ public class PersistenciaDeHabitacionesEnBaseDeDatosSQL implements PersistenciaD
             System.out.println("Error al recuperar de la base de datos "
                              + "la habitacion " + numeroDeHabitacion
                              + System.lineSeparator() + System.lineSeparator()
-                             + ex.getStackTrace().toString());
+                             + Arrays.toString(ex.getStackTrace()));
+        } finally {
+            try { if (respuesta != null) respuesta.close(); } catch (Exception ex) {}
+            try { if (comando != null) comando.close(); } catch (Exception ex) {}
         }
         return habitacion;
     }
 
     @Override
     public List<Habitacion> getTodas() {
-        // TODO Auto-generated method stub
-        return null;
+        java.util.ArrayList<Habitacion> habitaciones = new java.util.ArrayList<>();
+        StringBuilder sentenciaSQL = new StringBuilder();
+        sentenciaSQL.append("SELECT * FROM habitaciones");
+        sentenciaSQL.append(System.lineSeparator());
+        
+        java.sql.Statement comando = null;
+        java.sql.ResultSet respuesta = null;
+        try {
+            comando = baseDeDatos.createStatement();
+            respuesta = comando.executeQuery(sentenciaSQL.toString());
+            while (respuesta.next()) {
+                Habitacion habitacion;
+                habitacion = new Habitacion(java.util.UUID.fromString(respuesta.getString("idInterno")),
+                                            respuesta.getBoolean("activa"),
+                                            respuesta.getString("numeroDeHabitacion"),
+                                            TipoDeHabitacion.valueOf(respuesta.getString("tipo")),
+                                            TipoDeBaño.valueOf(respuesta.getString("tipoDeBaño"))
+                                            );
+                habitaciones.add(habitacion);
+            }
+        } catch(SQLException ex) {
+            System.out.println("Error al recuperar habitaciones de la base de datos "
+                             + System.lineSeparator() + System.lineSeparator()
+                             + Arrays.toString(ex.getStackTrace()));
+        } finally {
+            try { if (respuesta != null) respuesta.close(); } catch (Exception ex) {}
+            try { if (comando != null) comando.close(); } catch (Exception ex) {}
+        }
+        
+        return habitaciones;
     }
 
     @Override
